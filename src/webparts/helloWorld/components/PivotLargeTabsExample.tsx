bfb1c7a73198b202cd7059b3a1cb5b69e0c1ee8c
Rosus1691 <rohit.venkatakrishnan@gmail.com>
2021-01-09 19:15:22 +0530

    Initial Src commit

---
+++ src/webparts/helloWorld/components/PivotLargeTabsExample.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Pivot, PivotItem, PivotLinkFormat, PivotLinkSize } from 'office-ui-fabric-react/lib/Pivot';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+
+export default class PivotTabsLargeExample extends React.Component {
+  public  render() {
+    return (
+      <div>
+        <Pivot
+          aria-label="Links of Large Tabs Pivot Example"
+          linkFormat={PivotLinkFormat.tabs}
+          linkSize={PivotLinkSize.large}
+        >
+          <PivotItem headerText="Foo">
+            <Label>Pivot #1..</Label>
+          </PivotItem>
+          <PivotItem headerText="Bar">
+            <Label>Pivot #2</Label>
+          </PivotItem>
+          <PivotItem headerText="Bas">
+            <Label>Pivot #3</Label>
+          </PivotItem>
+          <PivotItem headerText="Biz">
+            <Label>Pivot #4</Label>
+          </PivotItem>
+        </Pivot>
+      </div>
+    );
+  }
+}
+
